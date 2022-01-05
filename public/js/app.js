66ecd670d297e51f69bf14e6e451de04e1102ce2
Parmvir-S <parmvir.shergill@gmail.com>
2022-01-05 15:12:36 -0800

    Setup App For Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ const messageOne = document.querySelector('#messageOne');
 const messageTwo = document.querySelector('#messageTwo');
 
 const getForecast = (location) => {
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
